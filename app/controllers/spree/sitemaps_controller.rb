ae85f455516165b57cdcddc8bf18078275638106
Neal Wong <neal.wkacc@gmail.com>
2022-02-03 10:45:41 -0500

    Added .gz sitemap process.

--- app/controllers/spree/sitemaps_controller.rb
+++ app/controllers/spree/sitemaps_controller.rb
@@ -2,13 +2,20 @@ module Spree
   class SitemapsController < Spree::StoreController
     def show
       filename = params[:sitemap]
-      if ::SpreeSitemap.serve_directly
-        # send_data ActiveStorage::Blob.service.download(filename), filename: filename, content_type: 'application/xml'
-        respond_to do |format|
-          format.xml { render body: ActiveStorage::Blob.service.download(filename) }
+      if ActiveStorage::Blob.service.exist?(filename)
+        if ::SpreeSitemap.serve_directly
+          if File.extname(filename).include?('.gz')
+            send_data ActiveStorage::Blob.service.download(filename), filename: filename, content_type: 'application/gzip'
+          else
+            respond_to do |format|
+              format.xml { render body: ActiveStorage::Blob.service.download(filename) }
+            end
+          end
+        else
+          return redirect_to ActiveStorage::Blob.service.url(params[:sitemap])
         end
       else
-        return redirect_to ActiveStorage::Blob.service.url(params[:sitemap])
+        render plain: '404 Not Found', status: 404
       end
     end
   end
