f9006c5b989eb08707795e2b2701853d46d1774a
Neal Wong <neal.wkacc@gmail.com>
2022-02-03 09:54:56 -0500

    Added SpreeSitemap.serve_directly option.

--- app/controllers/spree/sitemaps_controller.rb
+++ app/controllers/spree/sitemaps_controller.rb
@@ -1,7 +1,15 @@
 module Spree
   class SitemapsController < Spree::StoreController
     def show
-      return redirect_to ActiveStorage::Blob.service.url(params[:sitemap])
+      filename = params[:sitemap]
+      if ::SpreeSitemap.serve_directly
+        # send_data ActiveStorage::Blob.service.download(filename), filename: filename, content_type: 'application/xml'
+        respond_to do |format|
+          format.xml { render body: ActiveStorage::Blob.service.download(filename) }
+        end
+      else
+        return redirect_to ActiveStorage::Blob.service.url(params[:sitemap])
+      end
     end
   end
 end
--- lib/spree_sitemap.rb
+++ lib/spree_sitemap.rb
@@ -3,3 +3,11 @@ require 'sitemap_generator'
 require 'spree_sitemap/engine'
 require 'spree_sitemap/version'
 require 'spree_extension'
+
+module SpreeSitemap
+  @serve_directly = true
+
+  class << self
+    attr_accessor :serve_directly
+  end
+end
