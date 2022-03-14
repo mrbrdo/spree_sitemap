dd4c938c24ecda8ea29e0eae4f069fe0752d0ad5
Neal Wong <neal.wkacc@gmail.com>
2022-03-14 15:51:17 -0400

    Added missing route file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Spree::Core::Engine.add_routes do
   # Add your extension routes here
   get '/:sitemap', to: 'sitemaps#show', constraints: {sitemap: /sitemap\d*\.xml(\.gz)?/}
+  get '/robots.txt', to: 'robots#robots'
 end
